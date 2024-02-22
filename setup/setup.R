3a191502a655b2966e46158604355a3154799b73
Shannon Landovskis <shannonlandovskis@MacBook-Pro-3.local>
2024-02-22 12:26:58 -0400

    adding in proper website link, removing packages

--- setup/setup.R
+++ setup/setup.R
@@ -3,8 +3,6 @@
 ### R version: 3.6.x or newer (recommend 4.0.x) and RStudio.
 ### Windows users: Please also download the Rtools version compatible with your R version https://cran.r-project.org/bin/windows/Rtools/history.html (not for MacOS)
 
-# Advanced Telemetry Workshop ONLY: You must install also GDAL software, which can take a long time. See the extra setup document provided.
-
 # Once R/RStudio is installed: open RStudio and run this install script. Please run it line-by-line instead of all at once in case there are errors.
 
 #Note: When running through the installs, you may encounter a prompt asking you to upgrade dependent packages.
@@ -44,11 +42,11 @@ library(remotes)
 remotes::install_github('ocean-tracking-network/glatos@dev', force=TRUE)
                                                         
 ### Dataset and Code -----
-# Once the packages are installed, you can download the datasets and code for this workshop from https://github.com/ocean-tracking-network/otn-workshop-base/tree/master.
+# Once the packages are installed, you can download the datasets and code for this workshop from https://github.com/ocean-tracking-network/2024-GLATOS-intro-R-workshop.
 # 1) Select the GREEN "code" button at the top and choose "Download ZIP"
 # 2) Unzip the folder and move to secure location on your computer (Documents, Desktop etc.)
 # 3) Copy the folder's path and use it to set your working directly in R using `setwd('<path-to-folder>')`.
 
 # If you are familiar with Git and Github, feel free to clone this repository as you normally would,
-# by running `git clone https://github.com/ocean-tracking-network/otn-workshop-base/tree/master.git` in a terminal program
+# by running `git clone https://github.com/ocean-tracking-network/2024-GLATOS-intro-R-workshop.git` in a terminal program
 # and following from step 3 above.
